c8b0214e970fac0d95e8efb43a87add17c8a8147
gavin <tianyu_0723@hotmail.com>
2015-09-08 18:15:00 +0800

    G

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => env('DB_HOST', '121.42.158.246'),
+			'database'  => env('DB_DATABASE', 'dev_gavin_website'),
+			'username'  => env('DB_USERNAME', 'root'),
+			'password'  => env('DB_PASSWORD', 'Spark654231'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
