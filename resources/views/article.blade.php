f34d7deb131c63136ffcac021c50ca5870c8586c
gavin <tianyu_0723@hotmail.com>
2015-10-22 00:04:38 +0800

    1

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -5,6 +5,9 @@
 @stop
 
 @section('content')
+    <button id="fetch" type="button">获取</button>
+    <button id="fill" type="button">填充</button>
+
     <div id="summernote"></div>
 @stop
 
@@ -38,6 +41,41 @@
                     success: function(url) {
                         $('#summernote').summernote('editor.insertImage',url);
                     }
+                });
+            }
+
+
+
+
+            $('#fetch').click(function(){
+                console.log(encodeURIComponent($('#summernote').code()));
+            });
+
+            var str = "%3Cp%3E%E6%88%91%E5%93%88%E5%93%88%20'%E6%81%A9'%3C%2Fp%3E%3Cp%3E%22%E6%98%" +
+                    "AF%E5%90%97%22%3C%2Fp%3E%3Cp%3E%E6%81%A9%20%3Cspan%20style%3D%22background-color%" +
+                    "3A%20yellow%3B%22%3E%E6%98%AF%E7%9A%84%3C%2Fspan%3E%3C%2Fp%3E%3Col%3E%3Cli%3E%3Cspan%20sty" +
+                    "le%3D%22background-color%3A%20rgb(255%2C%20255%2C%200)%3B%22%3E%E9%A2%9D%3C%2Fspan%3E%3C%2F" +
+                    "li%3E%3Cli%3E%3Cspan%20style%3D%22background-color%3A%20rgb(255%2C%20255%2C%200)%3B%22%3E%E7%" +
+                    "9A%84%3C%2Fspan%3E%3C%2Fli%3E%3Cli%3E%3Cspan%20style%3D%22background-color%3A%20rgb(255%2C%2" +
+                    "0255%2C%200)%3B%22%3E%E6%98%AF%3C%2Fspan%3E%3C%2Fli%3E%3C%2Fol%3E%3Cp%3E%3Cbr%3E%3C%2Fp%3E%3Cp" +
+                    "%3E%E6%98%AF%E5%A3%AB%E5%A4%A7%E5%A4%AB7!%40%23%24%25%5E%26amp%3B*()%3C%2Fp%3E";
+            $('#fill').click(function(){
+                $('#summernote').code(decodeURIComponent(str));
+            });
+
+
+            function encodeUTF8(str){
+                var temp = "",rs = "";
+                for( var i=0 , len = str.length; i < len; i++ ){
+                    temp = str.charCodeAt(i).toString(16);
+                    rs  += "\\u"+ new Array(5-temp.length).join("0") + temp;
+                }
+                return rs;
+            }
+
+            function decodeUTF8(str){
+                return str.replace(/(\\u)(\w{4}|\w{2})/gi, function($0,$1,$2){
+                    return String.fromCharCode(parseInt($2,16));
                 });
             }
         });
